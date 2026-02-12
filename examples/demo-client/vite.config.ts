9db94bb861ed0ccfa0615ad03b766ea83f880274
FTWinston <ftwinston@gmail.com>
2026-02-12 09:22:51 +0000

    Add dedupe rule to demo-client vite config, to resolve error

--- examples/demo-client/vite.config.ts
+++ examples/demo-client/vite.config.ts
@@ -9,5 +9,10 @@ export default defineConfig({
     alias: {
       'demo-shared': path.resolve(__dirname, '../demo-shared'),
     },
+
+    // This is needed to prevent multiple versions of @colyseus/schema from being included in the bundle, which can cause issues with state synchronization.
+    // Specifically when using instanceof to check if an object is an ArraySchema, MapSchema, or Schema, it can fail if there are multiple versions of @colyseus/schema in the bundle.
+    // That happens for this project because it has @colyseus/schema as a dependency, but also imports the useRoomState hook from the root project, and that has its own dependency on (a separate instance of) @colyseus/schema.
+    dedupe: ['@colyseus/schema'],
   },
 })
