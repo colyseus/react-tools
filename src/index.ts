27a7209f28fd00ef8cd9c4b06b7d6c60fa0a9522
Endel Dreyer <endel.dreyer@gmail.com>
2026-02-25 18:26:11 -0300

    expose createRoomContext. bump 0.1.8

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { useRoomState } from './schema/useRoomState';
 export { useRoom } from './useRoom';
+export { createRoomContext } from './createRoomContext';
 export type { UseRoomResult } from './useRoom';
 export type { Snapshot } from './schema/createSnapshot';
